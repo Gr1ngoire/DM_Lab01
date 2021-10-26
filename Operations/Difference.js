dbe5dca36ee4b141db8d1b31557234a6038d804b
wilhelm wesser <wilhelmwesser228@gmail.com>
2021-10-27 00:09:51 +0300

    Difference bug fixed х2

--- Operations/Difference.js
+++ Operations/Difference.js
@@ -1,13 +1,13 @@
 import Utils from "./Utils.js";
 
 const difference = (A, B) => {
-    const resultArr = []
-    for (const el of A) {
-        if (!Utils.includesElement(el, B)) {
-            resultArr.push(el)
-        }
-    }
-    return resultArr
-    // return arr.filter((el) => {return !Utils.includesElement(el, U)})
+    // const resultArr = []
+    // for (const el of A) {
+    //     if (!Utils.includesElement(el, B)) {
+    //         resultArr.push(el)
+    //     }
+    // }
+    // return resultArr
+    return A.filter((el) => {return !Utils.includesElement(el, B)})
 }
 export default difference
